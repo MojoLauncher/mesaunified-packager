bf776705e52effa41d67ede0cbf3c0b3316441e3
Maksim Belov <ultramaksim@gmail.com>
2026-05-25 11:58:13 +0300

    Fix[aargen_mesa]: correctly declare map

--- aargen_mesa.sh
+++ aargen_mesa.sh
@@ -1,10 +1,10 @@
 
 declare -A archmap
 
-archmap["arm64-v8a"]   ="arm64"
-archmap["armeabi-v7a"] ="armv7"
-archmap["x86_64"]      ="x86_64"
-archmap["x86"]         ="x86"
+archmap["arm64-v8a"]="arm64"
+archmap["armeabi-v7a"]="armv7"
+archmap["x86_64"]="x86_64"
+archmap["x86"]="x86"
 
 makearchspec () {
    unzip -d ./aar-base/jni/$1 $PKGS_DIR/mesa-${archmap["$1"]}
